3b3027b6b77be99f8eee25bcdc5caedebb2e452f
Alexander <144883427+harr1er23@users.noreply.github.com>
2025-02-27 10:36:41 +0300

    Implement filters bar (#4)
    * file setting was comleted
    
    * base realization
    
    * implemented filters bar layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, TopBar } from "@/components/shared";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -9,6 +9,22 @@ export default function Home() {
       </Container>
 
       <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              Список товаров
+              {/* <ProductGroupList title="Пиццы" items={[1, 2, 3, 4, 5]}/>
+              <ProductGroupList title="Комбо" items={[1, 2, 3, 4, 5]}/> */}
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
   );
 }

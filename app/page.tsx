fb5b816aef40077e22abb1c76c0cb0bcc2a54e9a
Alexander <144883427+harr1er23@users.noreply.github.com>
2025-02-27 11:06:46 +0300

    Implemented products layout (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Filters, Title, TopBar } from "@/components/shared";
+import { Container, Filters, ProductsGroupList, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
@@ -11,16 +11,71 @@ export default function Home() {
       <TopBar />
 
       <Container className="mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           <div className="w-[250px]">
             <Filters />
           </div>
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              Список товаров
-              {/* <ProductGroupList title="Пиццы" items={[1, 2, 3, 4, 5]}/>
-              <ProductGroupList title="Комбо" items={[1, 2, 3, 4, 5]}/> */}
+              <ProductsGroupList title="Пиццы" categoryId={1} items={[{
+                id: 1,
+                name: "Чизбургер-пицца",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/0194491914e478b4aa3e18d44e07eed9.avif",
+                price: 499,
+                items: [{price: 499}]
+              },
+              {
+                id: 2,
+                name: "Маргарита",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/11ee7d6105ef6690b86fbde6150b5b0c.avif",
+                price: 550,
+                items: [{price: 550}]
+              },
+              {
+                id: 3,
+                name: "Гавайская",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/11ee7d617e9339cfb185921a343ad8fd.avif",
+                price: 450,
+                items: [{price: 450}]
+              },
+              {
+                id: 4,
+                name: "Четыре сезона",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/11ee7d611adf5aad898b8b651186e023.avif",
+                price: 490,
+                items: [{price: 490}]
+              }
+              ]}/>
+              <ProductsGroupList title="Завтрак" categoryId={1} items={[{
+                id: 1,
+                name: "Чизбургер-пицца",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/0194491914e478b4aa3e18d44e07eed9.avif",
+                price: 499,
+                items: [{price: 499}]
+              },
+              {
+                id: 2,
+                name: "Маргарита",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/11ee7d6105ef6690b86fbde6150b5b0c.avif",
+                price: 550,
+                items: [{price: 550}]
+              },
+              {
+                id: 3,
+                name: "Гавайская",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/11ee7d617e9339cfb185921a343ad8fd.avif",
+                price: 450,
+                items: [{price: 450}]
+              },
+              {
+                id: 4,
+                name: "Четыре сезона",
+                imageUrl: "https://media.dodostatic.net/image/r:292x292/11ee7d611adf5aad898b8b651186e023.avif",
+                price: 490,
+                items: [{price: 490}]
+              }
+              ]}/>
             </div>
           </div>
         </div>

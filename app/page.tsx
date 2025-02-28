1d8f73a5e799e4ebdb68c863aaa439346d74bf29
harr1er <harr1er.work23@gmail.com>
2025-02-28 19:58:42 +0300

    fix top bar

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
 
       </Container>
 
-      <TopBar />
+      <TopBar categories={categories.filter(category => category.products.length > 0)} />
 
       <Container className="mt-10 pb-14">
         <div className="flex gap-[80px]">
@@ -31,12 +31,12 @@ export default async function Home() {
           <div className="flex-1">
             <div className="flex flex-col gap-16">
               {categories.map(category => (
-                 category.products.length > 0 
-                 && <ProductsGroupList 
-                      key={category.id}
-                      title={category.name} 
-                      categoryId={category.id} 
-                      items={category.products}/>
+                category.products.length > 0
+                && <ProductsGroupList
+                  key={category.id}
+                  title={category.name}
+                  categoryId={category.id}
+                  items={category.products} />
               ))}
             </div>
           </div>

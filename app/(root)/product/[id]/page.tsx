375ffc18ca369fa19cd48e95eb1ec70ab9b63f38
harr1er <harr1er.work23@gmail.com>
2025-03-21 11:36:01 +0300

    fix

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -4,7 +4,11 @@ import { prisma } from "@/prisma/prisma-client";
 
 import { Container, Product} from "@/shared/components/shared";
 
-export default async function ProductPage({ params }: { params: Record<string, string> }) {
+interface ProductPageProps {
+    params: { id: string };
+}
+
+export default async function ProductPage({ params }: ProductPageProps) {
     const { id } = await Promise.resolve(params); 
     const product = await prisma.product.findFirst({ where: { id: Number(id) }, include: {
         ingredients: true,
